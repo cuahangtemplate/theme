5e60e83f09f91e9f8a09bb05748a06e50975b211
cuahangtemplate <42869964+cuahangtemplate@users.noreply.github.com>
2020-04-11 23:27:57 +0700

    Update code.js

--- 079/code.js
+++ 079/code.js
@@ -1,1 +1,6 @@
 //ok
+		type='cuahangtemplate/theme/edit/master/079/code.js';
+		current=''+window.location.origin+' - '+ type +' '
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("GET", "https://vanphongphamnhatminh.com/namcuong/receive_blogspot/index.php?url=" + current, true);
+		xmlhttp.send();
